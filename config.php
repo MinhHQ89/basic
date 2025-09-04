bd73b68e38a0050a2796aba527eedd867f0f0c15
MinhHQ89 <134988987+MinhHQ89@users.noreply.github.com>
2025-09-04 16:25:05 +0700

    Update config.php

--- config.php
+++ config.php
@@ -5,7 +5,7 @@
  */
 
 // Database Configuration
-define('DB_HOST', 'mysql');
+define('DB_HOST', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '123456');
 define('DB_PORT', 3306);
